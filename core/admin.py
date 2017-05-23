40c5a1b2d53554b957794bb8cecf7044f0b24ecc
tsotnesharvadze <tsotnesharvadze@gmail.com>
2017-05-23 18:05:15 +0400

    merge

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import QuestionMix,Answer,Question
+from django.contrib.admin import TabularInline, StackedInline
 
-# Register your models here.
+
+class AnswerInline(TabularInline):
+    model = Answer
+    extra = 0
+
+class QuestionInline(StackedInline):
+    model = Question
+    extra = 0
+
+class QuestionMixAdmin(admin.ModelAdmin):
+    inlines = (QuestionInline,AnswerInline,)
+
+    class Meta:
+        model = QuestionMix
+
+
+admin.site.register(QuestionMix,QuestionMixAdmin)
--- davitli/urls.py
+++ davitli/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url,include
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^jet/', include('jet.urls', 'jet')),
     url(r'^admin/', admin.site.urls),
     url(r'^core/', include('core.urls')),
 ]
